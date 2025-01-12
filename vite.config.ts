d51f3b08d1c8ade1ec199972f4ad47141a1263e8
Filip Tomanec <tomanec.f@gmail.com>
2025-01-12 13:51:38 +0100

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import {VitePWA} from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'PWA Test App',
+                short_name: 'PWATest',
+                description: 'My PWA Test App',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                display: 'standalone',
+                icons: [
+                    {
+                        "src": "/pwa-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/pwa-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/pwa-maskable-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "/pwa-maskable-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    }
+                ]
+            }
+        }),
+    ],
+})
